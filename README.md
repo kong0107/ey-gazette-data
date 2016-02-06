8a4c7beb3f4e712a8551d9cf886c0ddd76d4c5fa
Kong <Kong>
2016-02-07 00:59:08 +0800

    It works!

---
+++ README.md
@@ -0,0 +1,23 @@
+# ey-gazette
+下載行政院公報
+
+## Data Source
+檔案連結： `http://gazette.nat.gov.tw/egFront/OpenData/download.jsp?fn=<fn>`
+[行政院公報資訊網](http://gazette.nat.gov.tw/egFront/OpenData/help.jsp)
+
+> ## 本月資料
+> 提供最新一個月已出刊之公報壓縮檔，一天一個壓縮檔，…
+
+`<fn>` 格式為 `%03d-%02d-%02d` 。
+
+> ## 歷史資料
+> 提供行政院公報自民國94年度起各年度每月所出刊之公報壓縮檔，…
+> 此處壓縮檔非只含一天的公報，…
+> 各壓縮檔只包含該月份幾個出刊日的公報，依此類推，因此各月份都會有數量不等的壓縮檔，…
+
+`<fn>` 格式為 `%03d-%02d_%d` ，最後一個數字並不補零，且其前是底線而非引號。
+
+## Usage
+需要 BASH ，程式本體只有 `main.sh` 。
+如僅需下載一份，執行 `./main.sh <fn>` 。
+如需下載全部，則執行 `./main.sh` 。
--- main.sh
+++ main.sh
@@ -1,11 +1,82 @@
 #!/bin/bash
 #
-[ "$#" -eq 1 ] || (echo "Error: require one argument" && exit 1)
-curl -o $1.zip http://gazette.nat.gov.tw/egFront/OpenData/download.jsp?fn=$1
+# Usage: $0 <fn>
+# This would download `http://gazette.nat.gov.tw/egFront/OpenData/download.jsp?fn=<fn>`,
+# unzip it to `./data` if its size is big enough, and then delete the archive.
+# @see http://gazette.nat.gov.tw/egFront/OpenData/help.jsp
+#
+# Usage: $0
+# Download all and unzip them to `./data`.
+#
+# For bash syntax:
+# @see http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO.html
+# @see http://stackoverflow.com/questions/16489809/emulating-do-while-loop-in-bash
+
+#
+# Definitions.
+#
 minimumsize=400
-actualsize=`wc -c <$1.zip`
-if [ $actualsize -ge $minimumsize ]; then
-	unzip $1.zip -d data
-else
-    echo size too small, ignored.
+function grab {
+	curl -o $1.zip http://gazette.nat.gov.tw/egFront/OpenData/download.jsp?fn=$1
+	actualsize=`wc -c <$1.zip`
+	if [ $actualsize -ge $minimumsize ]; then
+		unzip $1.zip -d data
+		return 0
+	else
+		echo file too small, ignored
+		return 1
+	fi
+}
+
+if [ -n "$1" ]; then
+	grab $1
+	rm $1.zip
+	exit
 fi
+
+nowYear=$[`date +%Y`-1911]
+nowMonth=`date +%m`
+nowDate=`date +%d`
+
+#
+# A wrapper, for easily debug.
+#
+function wrapper {
+	grab $1
+	return $?
+}
+
+#
+# Download history data.
+#
+year=94
+while [ $year -le $nowYear ]; do
+	month=1
+	while [ $month -le 12 ]; do
+		if [[ $year -eq $nowYear && $month -eq $nowMonth ]]; then break; fi
+		date=1
+		while : ; do
+			str=`printf "%03d-%02d_%d" $year $month $date`
+			echo $str
+			wrapper $str
+			rm $1.zip
+			if [ $? -ne 0 ]; then break; fi
+			sleep 1
+			let date=date+1
+		done
+		let month=month+1
+	done
+	let year=year+1
+done
+
+#
+# Download data of the current month
+#
+date=1
+while [ $date -le $nowDate ]; do
+	str=`printf "%03d-%02d-%02d" $nowYear $nowMonth $date`
+	echo $str
+	wrapper $str
+	rm $1.zip
+	let date=date+1
+done
