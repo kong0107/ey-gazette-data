581131f5f7764d8c7337710e1929eb199856a2ef
Kong <Kong>
2016-02-08 20:06:04 +0800

    finishing import2mongo.js

--- import2mongo.js
+++ import2mongo.js
@@ -1,23 +1,47 @@
+/**
+ * @file 把已下載的 XML 轉換成 JS 物件、匯入 MongoDB 。
+ */
+
+/**
+ * MongoDB 資料庫 URL 。若設為偽，即不存取資料庫。
+ * @var {string} dburl
+ * @var {string} collName
+ */
+var dburl = 'mongodb://localhost:27017/ey-gazette';
+var collName = 'records';
+
+/**
+ * 設定是否輸出 JSON 檔。（將存於與各 XML 檔同一目錄）
+ * @var {boolean} outputJSON
+ */
+var outputJSON = false;
+
+/**
+ * 全域變數們。
+ */
 var util = require('util');
 var fs = require('fs');
 var xml2js = require('xml2js');
-var mongodb = require('mongodb');
-
 var coll;
 
-mongodb.MongoClient.connect('mongodb://localhost:27017/ey-gazette', function(err, db) {
-	//if(err) return console.error('Error: cannot connect database');
+if(dburl) require('mongodb').MongoClient.connect(dburl, function(err, db) {
+	if(err) return console.error('Error: cannot connect database');
+	coll = db.collection(collName);
+	main();
+});
+else main();
+
+//
+// Functions
+//
+function main() {
 	fs.readdir('./data', function(err, dirs) {
 		if(err) return console.error('Error: no `data` directory to import');
-		//coll = db.collection('records');
-
-		//dirs.forEach(parseByDate);
-
 		parseByDate(dirs, 0, function() {
 			console.log('Finish');
 		});
 	});
-});
+}
 
 function parseByDate(dates, index, callback) {
 	if(index == dates.length) return setImmediate(callback);
@@ -53,11 +77,15 @@ function parseByDate(dates, index, callback) {
 					});
 				}
 				catch(err) {return next('Error: xmlDoc with wrong structure ' + dateStr, err);}
-				//next(dateStr + ' ' + records.length);
-				fs.writeFile('test.json', JSON.stringify(records, null, '\t'), function(err) {
-					if(err) console.error(err);
-					else console.log('Success!');
+
+				if(outputJSON) fs.writeFileSync(
+					util.format('./data/%s/%s.json', dateStr, dateStr),
+					JSON.stringify(records, null, '\t').replace(/\n\t+/g, '\n')
+				);
+				if(coll) coll.insertMany(records, function(err) {
+					next('Finished importing gazette with date ' + dateStr, err);
 				});
+				else next(dateStr);
 			});
 		}
 	);
